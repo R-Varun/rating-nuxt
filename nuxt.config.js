ff1747676a5126e06802674b2a0b7c7b0e7f7c45
R-Varun <varun.rama@gmail.com>
2022-01-30 21:44:06 -0500

    modify port for heroku

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,34 +1,37 @@
 export default {
-  serverMiddleware: [{ path: '/api', handler: '~/server-middleware/api.js' }],
+  serverMiddleware: [{ path: "/api", handler: "~/server-middleware/api.js" }],
 
+  server: {
+    port: process.env.PORT || 3000,
+  },
   // Disable server-side rendering: https://go.nuxtjs.dev/ssr-mode
   ssr: false,
 
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
-    titleTemplate: '%s - Nuxt js UI Kit',
-    title: 'NuxtJs UI kit | Free UI kit built with Vuetifyy',
+    titleTemplate: "%s - Nuxt js UI Kit",
+    title: "NuxtJs UI kit | Free UI kit built with Vuetifyy",
     htmlAttrs: {
-      lang: 'en'
+      lang: "en",
     },
     meta: [
-      { charset: 'utf-8' },
-      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: '' },
-      { name: 'format-detection', content: 'telephone=no' }
+      { charset: "utf-8" },
+      { name: "viewport", content: "width=device-width, initial-scale=1" },
+      { hid: "description", name: "description", content: "" },
+      { name: "format-detection", content: "telephone=no" },
     ],
-    link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }]
+    link: [{ rel: "icon", type: "image/x-icon", href: "/favicon.ico" }],
   },
 
   // Global CSS: https://go.nuxtjs.dev/config-css
-  css: ['~/assets/scss/style.scss'],
+  css: ["~/assets/scss/style.scss"],
 
   // Customize the progress-bar color
   loading: {
-    color: 'DodgerBlue',
-    height: '5px',
+    color: "DodgerBlue",
+    height: "5px",
     continuous: true,
-    duration: 3000
+    duration: 3000,
   },
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
   plugins: [],
@@ -39,7 +42,7 @@ export default {
   // Modules for dev and build (recommended): https://go.nuxtjs.dev/config-modules
   buildModules: [
     // https://go.nuxtjs.dev/vuetify
-    '@nuxtjs/vuetify'
+    "@nuxtjs/vuetify",
   ],
 
   // Modules: https://go.nuxtjs.dev/config-modules
@@ -47,14 +50,14 @@ export default {
 
   // Vuetify module configuration: https://go.nuxtjs.dev/config-vuetify
   vuetify: {
-    customVariables: ['~/assets/scss/variables.scss'],
-    optionsPath: './vuetify.options.js',
+    customVariables: ["~/assets/scss/variables.scss"],
+    optionsPath: "./vuetify.options.js",
     treeShake: true,
     options: {
-      customProperties: true
-    }
+      customProperties: true,
+    },
   },
 
   // Build Configuration: https://go.nuxtjs.dev/config-build
-  build: {}
-}
+  build: {},
+};
